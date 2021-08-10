7feec651577ea7bea95c5e6bc03075d15ef66324
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-08-10 16:27:08 +0700

    DungxBuif ShoppingCart

--- Nhom8_ChuDu/Controllers/KhachSansController.cs
+++ Nhom8_ChuDu/Controllers/KhachSansController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
@@ -16,46 +17,87 @@ namespace Nhom8_ChuDu.Controllers
         private ChuDu24 db = new ChuDu24();
 
         // GET: KhachSans
-        public ActionResult Index(string sortOrder, string searchString, string currentFilter, int ?page)
-        {
-            //các biến sắp xếp
-            ViewBag.CurrentSort = sortOrder;
+        //public ActionResult Index(string sortOrder, string searchString, string currentFilter, int ?page)
+        //{
+        //    //các biến sắp xếp
+        //    ViewBag.CurrentSort = sortOrder;
 
+        //    ViewBag.SapTheoTen = String.IsNullOrEmpty(sortOrder) ? "ten_desc" : "";
+
+        //    //lấy giá trị của bộ lọc dữ liệu hiện tại
+        //    if (searchString != null)
+        //    {
+        //        page = 1;
+        //    }
+        //    else
+        //    {
+        //        searchString = currentFilter;
+        //    }
+        //    ViewBag.CurrentFilter = searchString;
+
+        //    var khachSans = db.KhachSans.Include(k => k.ThanhPho);
+
+        //    //lọc theo tên hàng
+        //    if (!String.IsNullOrEmpty(searchString))
+        //    {
+        //        khachSans = khachSans.Where(k => k.Ten.Contains(searchString));
+        //    }
+        //    switch (sortOrder)
+        //    {
+        //        case "ten_desc":
+        //            khachSans = khachSans.OrderByDescending(k => k.Ten);
+        //            break;
+        //        default:
+        //            khachSans = khachSans.OrderBy(k => k.Ten);
+        //            break;
+        //    }
+
+        //    khachSans = khachSans.OrderBy(k => k.IDKS);
+        //    int pageSize = 5;
+        //    int pageNumber = (page ?? 1);
+
+        //    return View(khachSans.ToPagedList(pageNumber,pageSize));
+        //}
+
+        public ActionResult Index(string sortOrder, string searchString, string curentFilter, int? page)
+        {
             ViewBag.SapTheoTen = String.IsNullOrEmpty(sortOrder) ? "ten_desc" : "";
 
-            //lấy giá trị của bộ lọc dữ liệu hiện tại
+            //Lấy gtri cảu bộ lọc hiện tại;
             if (searchString != null)
             {
                 page = 1;
             }
             else
             {
-                searchString = currentFilter;
+                searchString = curentFilter;
             }
-            ViewBag.CurrentFilter = searchString;
+            ViewBag.curentFilter = searchString;
 
-            var khachSans = db.KhachSans.Include(k => k.ThanhPho);
+            var khachsan = db.KhachSans.Select(p => p);
 
-            //lọc theo tên hàng
+            //Lọc theo tên hàng
             if (!String.IsNullOrEmpty(searchString))
             {
-                khachSans = khachSans.Where(k => k.Ten.Contains(searchString));
+                khachsan = khachsan.Where(p => p.Ten.Contains(searchString));
             }
+
+
             switch (sortOrder)
             {
                 case "ten_desc":
-                    khachSans = khachSans.OrderByDescending(k => k.Ten);
+                    khachsan = khachsan.OrderByDescending(s => s.Ten);
                     break;
                 default:
-                    khachSans = khachSans.OrderBy(k => k.Ten);
+                    khachsan = khachsan.OrderBy(s => s.Ten);
                     break;
             }
-
-            khachSans = khachSans.OrderBy(k => k.IDKS);
-            int pageSize = 5;
+            int pageSize = 3;
+            //Nếu trang null trả về 1
             int pageNumber = (page ?? 1);
 
-            return View(khachSans.ToPagedList(pageNumber,pageSize));
+            return View(khachsan.ToPagedList(pageNumber, pageSize));
+
         }
         public PartialViewResult _DsKhachSan()
         {
@@ -215,6 +257,218 @@ namespace Nhom8_ChuDu.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public ActionResult PhongKs(int IDKS, string sortOrder, string searchString, string curentFilter, int? page)
+        {
+            ViewBag.SapTheoTen = String.IsNullOrEmpty(sortOrder) ? "ten_desc" : "";
+
+            //Lấy gtri cảu bộ lọc hiện tại;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = curentFilter;
+            }
+            ViewBag.curentFilter = searchString;
+
+            var phongks = db.PhongKS.Where(p => p.IDKS.Equals(IDKS));
+
+            //Lọc theo tên hàng
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                phongks = phongks.Where(p => p.Ten.Contains(searchString));
+            }
+
+
+            switch (sortOrder)
+            {
+                case "ten_desc":
+                    phongks = phongks.OrderByDescending(s => s.Ten);
+                    break;
+                default:
+                    phongks = phongks.OrderBy(s => s.Ten);
+                    break;
+            }
+            int pageSize = 3;
+            //Nếu trang null trả về 1
+            int pageNumber = (page ?? 1);
+
+            return View(phongks.ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DatPhong(int IDPhongKS, int soluong, string sodienthoai)
+        {
+            // Nếu đã lưu sesion sdt thì thêm vào giỏ hàng và chuyển đến giỏi hàng
+            if (Session["sodienthoai"] != null)
+            {
+                List<DatPhong> dp = new List<DatPhong>();
+
+                if (Session["giohang"] != null)
+                {
+                    dp = (List<DatPhong>)Session["giohang"];
+                }
+
+                var datphong = new DatPhong();
+                datphong.IDPhongKS = IDPhongKS;
+                datphong.SoLuong = soluong;
+                datphong.SDT = sodienthoai;
+                dp.Add(datphong);
+
+                Session["giohang"] = dp;
+                return RedirectToAction("GioHang");
+            }
+
+            //Nếu điền số điện thoại thì thêm hàng và chueyenr đến giỏ hàng
+            if (!String.IsNullOrEmpty(sodienthoai))
+            {
+                Session["sodienthoai"] = sodienthoai;
+                List<DatPhong> dp = new List<DatPhong>();
+                var datphong = new DatPhong();
+                datphong.IDPhongKS = IDPhongKS;
+                datphong.SoLuong = soluong;
+                datphong.SDT = sodienthoai;
+                dp.Add(datphong);
+                Session["giohang"] = dp;
+                return RedirectToAction("GioHang");
+            }
+
+
+
+
+
+
+            // Nếu chưa điền số điện thoại bắt điền
+            ViewBag.IDPhongKS = IDPhongKS;
+            ViewBag.soluong = soluong;
+            return View();
+        }
+
+        public ActionResult DeleteGioHang(string IDPhongKS)
+        {
+            var bientam = new DatPhong();
+            List<DatPhong> a = new List<DatPhong>();
+            a = (List<DatPhong>)Session["gioHang"];
+            foreach (var item in a)
+            {
+                if (item.IDPhongKS == Int32.Parse(IDPhongKS))
+                {
+                    bientam = item;
+                }
+            }
+            a.Remove(bientam);
+            Session["gioHang"] = a;
+            return RedirectToAction("GioHang");
+        }
+
+        public ActionResult DeleteDonHang(string IDPhongKS)
+        {
+            var bientam = new DatPhong();
+            var sdt = Session["sodienthoai"].ToString();
+            var a = db.DatPhongs.Where(p => p.SDT.Equals(sdt));
+            foreach (var item in a)
+            {
+                if (item.IDPhongKS == Int32.Parse(IDPhongKS))
+                {
+                    bientam = item;
+                    break;
+                }
+            }
+
+            db.DatPhongs.Remove(bientam);
+            db.SaveChanges();
+            Session["gioHang"] = null;
+            return RedirectToAction("DonHang");
+        }
+
+        public ActionResult GioHang()
+        {
+            if (Session["giohang"] == null)
+            {
+                ViewBag.NoItems = "Chưa có phòng nào được đặt";
+
+            }
+            else
+            {
+                var dsphong = new ArrayList();
+                List<DatPhong> dp = (List<DatPhong>)Session["giohang"];
+                foreach (var item in dp)
+                {
+                    var tmpValue = db.PhongKS.Find(item.IDPhongKS);
+                    var gia = item.SoLuong * tmpValue.Gia;
+                    string[] tmp = { item.IDPhongKS.ToString(), tmpValue.Ten, item.SoLuong.ToString(), gia.ToString() };
+                    dsphong.Add(tmp);
+                }
+
+                ViewBag.gioHang = dsphong;
+            }
+
+
+            return View();
+        }
+
+
+
+        public ActionResult DonHang(string sodienthoaiSearch)
+        {
+            if (Session["sodienthoai"] == null && String.IsNullOrEmpty(sodienthoaiSearch))
+            {
+                ViewBag.NoSDT = "Nhập số điện thoại đẻ tìm kiếm phòng";
+                return View();
+            }
+            string sdt = "";
+            if (!String.IsNullOrEmpty(sodienthoaiSearch))
+            {
+                sdt = sodienthoaiSearch;
+            }
+            else
+            {
+                sdt = Session["sodienthoai"].ToString();
+            }
+
+            var phongdat = db.DatPhongs.Where(p => p.SDT.Equals(sdt));
+
+            if (phongdat.Count() == 0 && Session["giohang"] == null)
+            {
+                ViewBag.NoItems = "Chưa có phòng nào được đặt";
+                return View();
+            }
+
+            var dsphong = new ArrayList();
+            foreach (var item in phongdat)
+            {
+                var tmpValue = db.PhongKS.Find(item.IDPhongKS);
+                var gia = item.SoLuong * tmpValue.Gia;
+                string[] tmp = { item.IDPhongKS.ToString(), tmpValue.Ten, item.SoLuong.ToString(), gia.ToString() };
+                dsphong.Add(tmp);
+            }
+
+            if (Session["giohang"] != null)
+            {
+                List<DatPhong> dp = (List<DatPhong>)Session["giohang"];
+                foreach (var item in dp)
+                {
+                    db.DatPhongs.Add(item);
+                    db.SaveChanges();
+                    var tmpValue = db.PhongKS.Find(item.IDPhongKS);
+                    var gia = item.SoLuong * tmpValue.Gia;
+                    string[] tmp = { item.IDPhongKS.ToString(), tmpValue.Ten, item.SoLuong.ToString(), gia.ToString() };
+                    dsphong.Add(tmp);
+                }
+
+                Session["giohang"] = null;
+            }
+
+            ViewBag.gioHang = dsphong;
+
+
+
+
+            return View();
         }
     }
 }
